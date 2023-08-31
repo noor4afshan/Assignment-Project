863427e78293718316b628a3bd85e7273577a550
Noor <noorafshanparveen955@gmail.com>
2023-08-31 19:34:05 +0530

    change

--- src/router.js
+++ src/router.js
@@ -1,17 +1,15 @@
-import {createRouter,createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import userList from './components/userList.vue'
 import userDetails from './components/userDetails.vue'
 import editUser from './components/editUser.vue'
 
 
 const routes = [
-   
-    { path: '/', name: 'userList', component: userList },
-    { path: '/userDetails/:id', name: 'userDetails', component: userDetails, props: true},
-    { path: '/editUser/:id', name: 'editUser', component: editUser},
 
-    
-    
+    { path: '/', name: 'userList', component: userList },
+    { path: '/userDetails/:id', name: 'userDetails', component: userDetails, props: true },
+    { path: '/editUser/:id', name: 'editUser', component: editUser },
+
 ]
 const router = createRouter({
     history: createWebHistory(),
